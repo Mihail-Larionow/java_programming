e6f661902d3fa57442c84b72e803ef7a90285571
Mihail <mihail.larionow2001@gmail.com>
2023-03-11 19:51:31 +0300

    Lab_1 is started

---
+++ Lab_1/JavaApplication1/src/main/java/com/michel/javaapplication1/JavaApplication1.java
@@ -0,0 +1,12 @@
+package com.michel.javaapplication1;
+
+/**
+ *
+ * @author mihai
+ */
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
