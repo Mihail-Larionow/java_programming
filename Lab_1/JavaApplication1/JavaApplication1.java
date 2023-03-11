0e0f328381ed95096dd49d12764002b8ebc56769
Mihail <mihail.larionow2001@gmail.com>
2023-03-11 21:49:09 +0300

    Lab_1 is done

---
+++ Lab_1/JavaApplication1/JavaApplication1.java
@@ -0,0 +1,15 @@
+package com.michel.javaapplication1;
+
+/**
+ *
+ * @author mihai
+ */
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int d = 7;
+        int sum = a + d;
+        System.out.println("Sum = " + sum);
+    }
+}
