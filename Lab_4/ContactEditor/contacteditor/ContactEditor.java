dddf4303c6e75050f831def907cd2f548478d1a1
Mihail <mihail.larionow2001@gmail.com>
2023-03-15 14:58:00 +0300

    Lab_4 is done

---
+++ Lab_4/ContactEditor/contacteditor/ContactEditor.java
@@ -0,0 +1,12 @@
+package com.michel.contacteditor;
+
+/**
+ *
+ * @author mihai
+ */
+public class ContactEditor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
